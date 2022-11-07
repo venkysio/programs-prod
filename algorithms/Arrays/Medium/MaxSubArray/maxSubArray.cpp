905239841934ede3cf01a3b2f4b8010adaf69ceb
Daali <daalivardhan55@gmail.com>
2022-11-08 01:05:10 +0530

    changes

---
+++ algorithms/Arrays/Medium/MaxSubArray/maxSubArray.cpp
@@ -0,0 +1,10 @@
+// cpp file 
+
+// def maxSubArray(self, nums: List[int]) -> int:
+//         cursum=0
+//         maxsum=nums[0]
+//         for i in range(len(nums)):
+//             cursum=max(cursum+nums[i],nums[i])
+//             maxsum=max(maxsum,cursum)
+//         return maxsum
+        
---
+++ algorithms/Arrays/Medium/MaxSubArray/maxSubArray.java
@@ -0,0 +1,13 @@
+// java file 
+
+
+// public class maxSubArray {
+//     def maxSubArray(self, nums: List[int]) -> int:
+//         cursum=0
+//         maxsum=nums[0]
+//         for i in range(len(nums)):
+//             cursum=max(cursum+nums[i],nums[i])
+//             maxsum=max(maxsum,cursum)
+//         return maxsum
+        
+// }
