ed4274f7a515b1c40f48fcebb5310c878986a08e
Daali <daalivardhan55@gmail.com>
2022-11-13 15:14:56 +0530

    simplesort complete

--- algorithms/Sorting/Easy/SimpleSort/VSDSimpleSort.cpp
+++ algorithms/Sorting/Easy/SimpleSort/VSDSimpleSort.cpp
@@ -1,1 +1,33 @@
 
+
+#include<iostream>
+
+using namespace std;
+
+void swap(int arr[],int i,int j){
+    int temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+}
+
+void VSDSimpleSort(int arr[],int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(arr[i]<arr[j]){
+                swap(arr,i,j);
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+
+int main(){
+    int n=7;
+    int arr[]={2,24,12,96, 456, 899,34};
+    VSDSimpleSort(arr,n);
+    return 0;
+}
--- algorithms/Sorting/Easy/SimpleSort/VSDSimpleSort.py
+++ algorithms/Sorting/Easy/SimpleSort/VSDSimpleSort.py
@@ -1,1 +1,12 @@
 
+def VSDSimpleSort(arr,n):
+    for i in range(n):
+        for j in range(n):
+            if(arr[i]<arr[j]):
+                arr[i],arr[j]=arr[j],arr[i]
+    print(*arr,sep=" ")
+
+if __name__=="__main__":
+    n=7
+    arr=[2,24,12,96, 456, 899,34]
+    VSDSimpleSort(arr,n)
