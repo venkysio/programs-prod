076d8d756ecf59b4dc2fd21ff90e952e8e425e89
Sai Krishna <76392114+vemulasaikrishna03@users.noreply.github.com>
2022-11-13 18:51:14 +0530

    Add files via upload

---
+++ algorithms/Strings/Easy/ValidPalindrome/VSDValidPalindrome.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+using namespace std;
+
+
+void VSDcheck_palindrome(string str){
+    int len=str.length();
+    int ptr=1;
+    for(int i=0;i<len;i++){
+        if(str[i]!=str[len-i-1]){
+            ptr=0;
+            break;
+        }
+    }
+    if(ptr){
+        cout<<str<<" is a palindrome";
+        
+    }
+    else{
+        cout<<str<<" is not a palindrome";
+    }
+
+}
+int main() {
+	int VSD_test_count;
+    cout<<"enter no of test case count ";
+    cin>>VSD_test_count;
+    while(VSD_test_count)
+    {
+		cout<<"enter a string to check palindrome ";
+    string str;
+    cin>>str;
+    cout<<endl;
+    VSDcheck_palindrome(str);
+    VSD_test_count--;
+    }
+	return 0;
+	
+}
