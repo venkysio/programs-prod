0910e3ec3174248ec9724de7648ae320e43320fa
Sai Krishna <76392114+vemulasaikrishna03@users.noreply.github.com>
2022-11-13 19:02:17 +0530

    Add files via upload

--- algorithms/Strings/Easy/FizzBuzz/VSDFizzBuzz.cpp
+++ algorithms/Strings/Easy/FizzBuzz/VSDFizzBuzz.cpp
@@ -1,36 +1,38 @@
-#include<iostream>
-#include<string>
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<string> fizzbuzz(int n){
-    vector<string> ans;
-    for(int i=1;i<=n;i++){
-        if (i%3==0 && i%5==0){
-            ans.push_back("FizzBuzz");
+ vector<string> VSD_fizz_Buzz(int n) {
+        
+        vector<string> VSD;
+        for(int i=1; i<=n; i++)
+        {
+            if(i%3 == 0 && i%5 == 0)
+                VSD.push_back("FizzBuzz");
+            else if(i%3 == 0)
+                VSD.push_back("Fizz");
+            else if(i%5 == 0)
+                VSD.push_back("Buzz");
+            else
+                VSD.push_back(to_string(i));
         }
-        else if (i%3==0){
-            ans.push_back("Fizz");
-        }
-        else if (i%5==0){
-            ans.push_back("Buzz");
-        }
-        else{
-            string x;
-            x=to_string(i);
-            ans.push_back(x);
-        }
+        return VSD;
     }
-    return ans;
 
-
-
-}
-
-int main(){
-    vector<string> ans=fizzbuzz(15);
-    for(auto& a:ans){
-        cout<<a<<" ";
+int main() {
+	int VSD_test_count;
+    cout<<"enter no of test case count ";
+    cin>>VSD_test_count;
+    while(VSD_test_count)
+    {
+		cout<<"enter a number";
+    int n;
+    cin>>n;
+    cout<<endl;
+    vector<string>v=VSD_fizz_Buzz(n);
+	for (auto i = v.begin(); i != v.end(); ++i)
+        cout << *i << " ";
+    VSD_test_count--;
     }
+	return 0;
+	
 }
