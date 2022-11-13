d9206c1c2cf97fb64ee5a20ce83497e3276a7019
SarvaniViswanadha <82228093+SarvaniViswanadha@users.noreply.github.com>
2022-11-13 17:43:47 +0530

    Update VSDRemoveVowelsFromAString.java

---
+++ algorithms/Strings/Easy/RemoveVowelsFromAString/VSDRemoveVowelsFromAString.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class VSDRemoveVowelsFromAString {
+	//Function to take user input
+		public static String VSDuserInput() {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter string");
+			String s=sc.next();
+			return s;
+		}
+		//Function to remove vowels from the given String
+		public static String VSDRemoveVowels(String s) {
+			String output="";
+			for(int i=0;i<s.length();i++) {
+				if(s.charAt(i)=='A'||s.charAt(i)=='a'||
+						s.charAt(i)=='E'||s.charAt(i)=='e'||
+						s.charAt(i)=='I'||s.charAt(i)=='i'||
+						s.charAt(i)=='O'||s.charAt(i)=='o'||
+						s.charAt(i)=='U'||s.charAt(i)=='u') {
+					continue;
+				}
+				else {
+					output+=s.charAt(i);
+				}
+			}
+			return output;
+		}
+		//Function to print output
+		public static void VSDprintOutput(String out) {
+			System.out.println("The string without vowels is:");
+				System.out.println(out);
+				
+			}
+public static void main(String args[]) {
+	String s=VSDuserInput();//Taking user input
+	String out=VSDRemoveVowels(s);
+	VSDprintOutput(out);
+}
+}
