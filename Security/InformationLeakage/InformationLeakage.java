12e5ebd42c76e4fb23254dad6ad31c36265d0006
SarvaniViswanadha <82228093+SarvaniViswanadha@users.noreply.github.com>
2022-12-01 13:12:04 +0530

    Add files via upload

---
+++ Security/InformationLeakage/InformationLeakage.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+
+public class InformationLeakage {
+   static final String DB_URL = " jdbc:mysql://localhost:3306/sakila";
+   static final String USER = "root";
+   static final String PASS = "viswa123";
+   static final String QUERY = "SELECT id, first, last, age FROM actor";
+
+   public static void main(String[] args) {
+      // Open a connection
+	  String status="";
+      try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+         Statement stmt = conn.createStatement();
+         ResultSet rs = stmt.executeQuery(QUERY);) {
+         // Extract data from result set
+         while (rs.next()) {
+            System.out.println("FirstName:" + rs.getString("first_name"));
+            System.out.println("LastName" + rs.getString("last_name"));
+         }
+      } catch (SQLException se) {
+status =" failed\r\n";
+status += se;
+}catch (Exception e) {
+status = e.toString();
+}
+if (!(status.equals("No"))) {
+System.out.println(status);
+}
+   }
+}
