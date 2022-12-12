c5b40f32ac0a3431f24a65392deceea10859f9b5
SarvaniViswanadha <82228093+SarvaniViswanadha@users.noreply.github.com>
2022-12-12 12:43:13 +0530

    Add files via upload

---
+++ Security/CrossSiteScripting/XSS-Server/XSS-nonpersistent/XSS-cookies/src/main/java/Trail.java
@@ -0,0 +1,43 @@
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.io.*;
+import java.sql.*;
+public class Trail extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter pw = res.getWriter();
+        pw.println("<html><body>");
+        String uname = req.getParameter("uname");
+        Cookie ckarr[] = req.getCookies();
+        int flag = 0;
+        if (ckarr != null) {
+            for (Cookie ck : ckarr) {
+                if (ck.getName().equals(uname)) {
+                    pw.println("Welcome "+uname);
+                    pw.println(
+                            "<HTML>\n" +
+                                    "<form id=\"form\" name=\"form\" method=\"get\" action=\"search\">" +
+                                    "<button type=\"submit\">GO TO SEARCH WEBSITE</button> " +
+                                    "</HTML>");
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        if (flag == 0) {
+            pw.println(
+                    "<HTML>\n" +
+
+                            "<H1>" + "Login again" + "</H1>\n" +
+
+                            "<form id=\"form\" name=\"form\" method=\"get\" action=\"givepassword\">" +
+                            "Enter user name: <input type=\"text\" name=\"uname\"/>" +"<br>"+
+                            "Enter Password: <input type=\"text\" name=\"pass\"/>" +
+                            "<button type=\"submit\">Submit</button> " +
+
+                            "</HTML>");
+        }
+    }
+}
+
