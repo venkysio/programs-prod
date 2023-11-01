45f25c384b440d70ad99408ea0807b94fea605f5
Venky Karukuri <venky@vsdevelopers.io>
2023-11-01 17:13:31 +0530

    Added comments

--- algorithms/Graphs/Medium/DetectingCycleInAGraph/VSDDetectingCycleInAGraph.cpp
+++ algorithms/Graphs/Medium/DetectingCycleInAGraph/VSDDetectingCycleInAGraph.cpp
@@ -1,6 +1,22 @@
+
+/* Copyrights to venkys.io
+For more information, visit https://venkys.io */
+
+// C++ program to find a cycle in a graph using DFS
+// Stable: Yes
+// Inplace: No
+// Adaptive: No
+
+// Space complexity: O(V+E)
+// Time complexity:
+//| Algorithm | Best | Average | Worst     |
+//|-----------|------|---------|-------    |
+//| GraphCycle| Ω(V) | Θ(V + E)| O(V + E)  |
+//|-----------|------|---------|-----------|
+
 #include<bits/stdc++.h>
 
-
+// function to perform DFS
 bool dfs(int node,std::vector<std::vector<int>>& graph,bool visited[],int parent){
     visited[node]=true;
     for(int neighbour:graph[node]){
@@ -14,25 +30,32 @@ bool dfs(int node,std::vector<std::vector<int>>& graph,bool visited[],int parent
     return false;
 }
 
+// Function to check if there is a cycle in the graph
 bool isCycle(std::vector<std::vector<int>>& graph){
+    // get the number of vertices
     int v=graph.size();
+    // create a visited array
     bool visited[v]{false};
     
+    // call the dfs function for each node
     for(int i=0;i<v;i++){
-        if(!visited[i] && dfs(i,graph,visited,-1))
+        if(!visited[i] && dfs(i,graph,visited,-1)) //if the node is not visited and there is a cycle
             return true ;
     
     }
     return false;
 }
 
+// main function
 int main(){
+    // graph is represented as an adjacency list
     std::vector<std::vector<int>> graph={
         {1,2},
         {0,3},
         {0},
         {1}
     };
+    // print the result of the function
     std::cout<<isCycle(graph);
     return 0;
 }
